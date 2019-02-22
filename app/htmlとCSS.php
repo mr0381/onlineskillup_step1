727adbb0d81ff789d9715356e042b03c9a244222
mr0381 <mai.clarinet.0523@gmail.com>
2019-02-23 03:07:26 +0900

    最初のコミット

---
+++ app/htmlとCSS.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>html‚ÆCSS</title>
+    <link rel="stylesheet" href="html.css">
+  </head>
+  <body>
+    <h1>HTML‚ÆCSS‚É‚Â‚¢‚Ä</h1>
+    <p class="sample">ƒtƒH[ƒ€‚ğì‚é</p>
+    <form action="/my-handling-form-page" method="post">
+      <div>
+        <label for="msg">Message:</label>
+        <textarea id="msg" name="user_message"></textarea>
+      </div>
+      <div class="button">
+        <button type="submit">Send your message</button>
+      </div>
+    </form>
+  </body>
+</html>
